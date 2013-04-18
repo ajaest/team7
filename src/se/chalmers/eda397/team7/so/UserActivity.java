60e2c6f62abbf18c22fc426a519b3484c8422c1e
Javi <Javi@192.36.224.77>
2013-04-18 17:28:32 +0200

    First drafts for the userList screen and the user screen

---
+++ src/se/chalmers/eda397/team7/so/UserActivity.java
@@ -0,0 +1,24 @@
+package se.chalmers.eda397.team7.so;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class UserActivity extends Activity {
+	TextView tv1;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user);
+		tv1 = (TextView) findViewById(R.id.textViewLocation);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.user, menu);
+		return true;
+	}
+
+}
