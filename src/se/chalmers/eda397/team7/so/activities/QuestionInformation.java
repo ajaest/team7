2a07c301b85c2b9db016142e78523aa339e5760a
Ally Bitebo <ezedinho@gmail.com>
2013-04-18 13:30:23 +0200

    created new layout for question info

---
+++ src/se/chalmers/eda397/team7/so/activities/QuestionInformation.java
@@ -0,0 +1,29 @@
+package se.chalmers.eda397.team7.so.activities;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class QuestionInformation extends Activity{
+	
+	//UI reference
+	private TextView QuestionHeader;
+	private TextView QuestionContent;
+	private TextView AcceptedAnswer;
+	private TextView AcceptedAnswerContent;
+	private Button SeeAllAnswers;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		QuestionHeader = (TextView) findViewById(R.id.tvQuestionHeader);
+		
+	}
+	
+	
+	
+
+}
